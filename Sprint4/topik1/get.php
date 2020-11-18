162a29c57a572e718e9ee103c9442fde3e5de67e
shofiyyahsayyidahhauro <juneshofi@gmail.com>
2020-11-18 14:13:36 +0700

    topik1

--- Sprint4/topik1/get.php
+++ Sprint4/topik1/get.php
@@ -4,12 +4,13 @@
     <title>percobaan</title>
     <body>
       <form method="get" action="action2.php">
-        <input type="text" name="nama_kurir" placeholder="masukkan nama kurir">
-        <input type="text" name="nama_pengirim" placeholder="masukkan nama pengirim">
-        <input type="text" name="asal_barang" placeholder="asal barang">
-        <input type="text" name="tujuan_barang" placeholder="tujuan barang">
-        <input type="submit" value="submit">
-
+      <table border="1" bordercolor="#ff00ff">
+      <tr><td><input type="text" name="nama_kurir" placeholder="masukkan nama kurir"><tr><td>
+      <tr><td><input type="text" name="nama_pengirim" placeholder="masukkan nama pengirim"><tr><td>
+      <tr><td><input type="text" name="asal_barang" placeholder="asal barang"><tr><td>
+      <tr><td><input type="text" name="tujuan_barang" placeholder="tujuan barang"><tr><td>
+      <tr><td><input type="submit" value="submit"><tr><td>
+      </table>
       </form>
     </body>
   </head>
--- Sprint4/topik1/post.php
+++ Sprint4/topik1/post.php
@@ -4,9 +4,11 @@
     <title>percobaan</title>
     <body>
       <form method="post" action="action1.php">
-        <input type="text" name="nama_barang" placeholder="masukkan nama">
-        <input type="text" name="harga" placeholder="masukkan harga">
-        <input type="submit" value="submit">
+      <table border="1" bordercolor="#ff00ff">
+       <tr><td><input type="text" name="nama_barang" placeholder="masukkan nama"></td></tr>
+        <tr><td><input type="text" name="harga" placeholder="masukkan harga"></td></tr>
+        <tr><td><input type="submit" value="submit"></td></tr>
+              </table>
       </form>
     </body>
   </head>
