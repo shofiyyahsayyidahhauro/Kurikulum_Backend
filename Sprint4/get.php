6b2a9e1f07e0ad66ac56cc4704a170cd671f3222
shofiyyahsayyidahhauro <juneshofi@gmail.com>
2020-11-18 08:35:51 +0700

    get and post

---
+++ Sprint4/get.php
@@ -0,0 +1,16 @@
+
+<html>
+  <head>
+    <title>percobaan</title>
+    <body>
+      <form method="get" action="action2.php">
+        <input type="text" name="nama_kurir" placeholder="masukkan nama kurir">
+        <input type="text" name="nama_pengirim" placeholder="masukkan nama pengirim">
+        <input type="text" name="asal_barang" placeholder="asal barang">
+        <input type="text" name="tujuan_barang" placeholder="tujuan barang">
+        <input type="submit" value="submit">
+
+      </form>
+    </body>
+  </head>
+</html>
---
+++ Sprint4/post.php
@@ -0,0 +1,17 @@
+
+<html>
+  <head>
+    <title>percobaan</title>
+    <body>
+      <form method="post" action="action1.php">
+        <input type="text" name="nama_barang" placeholder="masukkan nama">
+        <input type="text" name="harga" placeholder="masukkan harga">
+        <input type="submit" value="submit">
+      </form>
+    </body>
+  </head>
+</html>
+
+
+
+
