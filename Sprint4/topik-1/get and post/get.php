dcdbc3c079c53db49a561f2e1870f7642b513ab3
shofiyyahsayyidahhauro <juneshofi@gmail.com>
2020-11-28 10:14:44 +0700

    topik1

---
+++ Sprint4/topik-1/get and post/get.php
@@ -0,0 +1,17 @@
+
+<html>
+  <head>
+    <title>percobaan</title>
+    <body>
+      <form method="get" action="action2.php">
+      <table border="1" bordercolor="#ff00ff">
+      <tr><td><input type="text" name="nama_kurir" placeholder="masukkan nama kurir"><tr><td>
+      <tr><td><input type="text" name="nama_pengirim" placeholder="masukkan nama pengirim"><tr><td>
+      <tr><td><input type="text" name="asal_barang" placeholder="asal barang"><tr><td>
+      <tr><td><input type="text" name="tujuan_barang" placeholder="tujuan barang"><tr><td>
+      <tr><td><input type="submit" value="submit"><tr><td>
+      </table>
+      </form>
+    </body>
+  </head>
+</html>
