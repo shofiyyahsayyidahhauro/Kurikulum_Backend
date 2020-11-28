e2c2477d95743889f38a5805a470cd2815ff3606
shofiyyahsayyidahhauro <juneshofi@gmail.com>
2020-11-28 10:36:34 +0700

    sprint1-4

---
+++ Sprint-4/topik1/get and post/post.php
@@ -0,0 +1,19 @@
+
+<html>
+  <head>
+    <title>percobaan</title>
+    <body>
+      <form method="post" action="action1.php">
+      <table border="1" bordercolor="#ff00ff">
+       <tr><td><input type="text" name="nama_barang" placeholder="masukkan nama"></td></tr>
+        <tr><td><input type="text" name="harga" placeholder="masukkan harga"></td></tr>
+        <tr><td><input type="submit" value="submit"></td></tr>
+              </table>
+      </form>
+    </body>
+  </head>
+</html>
+
+
+
+
---
+++ Sprint-4/topik2/session/app.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['username'])){
+        header("Location: login.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <h1>Selamat, anda berhasil login</h1>
+    <a href="action-logout.php">Logout</a>
+</center>
+</body>
+</html>
