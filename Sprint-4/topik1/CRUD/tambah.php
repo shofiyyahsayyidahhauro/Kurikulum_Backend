2c18e90b345c884021f7a108b7d186ffa998366a
shofiyyahsayyidahhauro <juneshofi@gmail.com>
2020-12-01 10:18:17 +0700

    CRUD

---
+++ Sprint-4/topik1/CRUD/tambah.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD PHP dan MySQLi</title>
+</head>
+<body>
+
+	<h2>CRUD DATA BARANG/h2>
+	<br/>
+	<a href="index.php">KEMBALI</a>
+	<br/>
+	<br/>
+	<h3>TAMBAH</h3>
+	<form method="post" action="tambah_aksi.php">
+		<table>
+			<tr>			
+				<td>Nama</td>
+				<td><input type="text" name="nama_barang"></td>
+			</tr>
+			<tr>
+				<td>deskripsi</td>
+				<td><input type="text" name="deskripsi_barang"></td>
+			</tr>
+			<tr>
+				<td>Tempat</td>
+				<td><input type="text" name="tempat_kulakan"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="submit" value="SIMPAN"></td>
+			</tr>		
+		</table>
+	</form>
+</body>
+</html>
