5d24b604a1d24a1c66ab9112f0ab4537ec2a1c4a
shofiyyahsayyidahhauro <juneshofi@gmail.com>
2020-12-26 09:40:07 +0700

    update

--- blog_Shofi/blog_addpost.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('Blog.php'); // include Blog.php file
-$blog = new Blog(); 
-$blog->blog_create(); // memanggil fungsi blog_create()
-?>
--- blog_Shofi/blog_delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('Blog.php'); // include Blog.php file
-
-$blog = new Blog();  
-$blog->blog_delete($_GET['id']); 
-?>
